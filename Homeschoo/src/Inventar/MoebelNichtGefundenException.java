8984921dea4531e6d6565076b83bb4a9f0437895
ericl <eric.l3@schule.bremen.de>
2021-07-04 23:13:31 +0200

    hnz

---
+++ Homeschoo/src/Inventar/MoebelNichtGefundenException.java
@@ -0,0 +1,15 @@
+package Inventar;
+
+public class MoebelNichtGefundenException extends Exception {
+
+
+    private String inventarNr;
+
+    public MoebelNichtGefundenException(String inventarNr) {
+        this.inventarNr = inventarNr;
+    }
+
+    public String getMessage() {
+        return "Moebel mit der Nummer " + this.inventarNr + " nicht gefunden";
+    }
+}
--- src/ztBot.java
+++ src/ztBot.java
@@ -1,3 +1,11 @@
+import java.io.IOException;
+
 public class ztBot {
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("sag alter vallah");
+        int alter = IO.readInteger();
+        System.out.println("du bist" +alter +" jahre alt du huan");
+    }
 
 }
